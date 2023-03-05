6fc53c0a42c9525c923a19bf6b1318902b89b21e
PaoloPP16 <pahama2018@gmail.com>
2023-03-05 17:03:33 -0500

    PirmerGitModales

---
+++ src/components/modalSetting.js
@@ -0,0 +1,14 @@
+import Modal from "./modal";
+export default function ModalSetting({title,root}){
+    
+    
+    
+    return(
+        <Modal title={title} root={root}>
+            modal de configuracion
+        </Modal>
+    );
+
+
+
+}
